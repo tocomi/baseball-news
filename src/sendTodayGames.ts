eb30129038beaa0651bb5d2049baf56f42a4a463
tocomi <tocomi0112@gmail.com>
2024-02-24 00:32:02 +0900

    feat: 順位表の送信

--- src/sendTodayGames.ts
+++ src/sendTodayGames.ts
@@ -1,27 +1,23 @@
-import { Browser, Page } from "puppeteer";
-import { postImage, postMessage } from "./slack/message";
-import { setup } from "./puppeteer/setup";
-import { SectionBlock } from "@slack/web-api";
+import { Browser, Page } from 'puppeteer';
+import { postImage, postMessage } from './slack/message';
+import { setup } from './puppeteer/setup';
+import { SectionBlock } from '@slack/web-api';
 
 /**
  * 当日の試合がある場合は true を返す
  */
-const noTodayGames = async ({
-  page,
-}: {
-  page: Page;
-}): Promise<boolean> => {
+const noTodayGames = async ({ page }: { page: Page }): Promise<boolean> => {
   // NOTE: 「XXはありません」の要素
   const element = await page.$('.bb-noData');
-  if (!element) return false
+  if (!element) return false;
 
   // NOTE: 「記事はありません」のパターンもあるのでテキストまで確認
   const textContent = await (
     await element.getProperty('textContent')
   ).jsonValue();
-  if (!textContent) return false
-  return textContent.includes('試合はありません')
-}
+  if (!textContent) return false;
+  return textContent.includes('試合はありません');
+};
 
 async function sendTodayGames({
   page,
@@ -30,38 +26,59 @@ async function sendTodayGames({
   page: Page;
   browser: Browser;
 }) {
-  const url = 'https://baseball.yahoo.co.jp/npb';
-  await page.goto(url);
+  const scoreUrl = 'https://baseball.yahoo.co.jp/npb';
+  await page.goto(scoreUrl);
 
-  const element = await page.$('.bb-score');
-  if (!element) {
-    console.error('Element not found');
-    await browser.close();
-    return;
-  }
-
-  if (await noTodayGames({page})) {
+  // 試合の有無のチェック
+  if (await noTodayGames({ page })) {
     console.log('🚌 No Game today.');
     await postMessage({
-      blocks: [{
-        type: 'section',
-        text: {
-          type: 'mrkdwn',
-          text: `🐨 *本日の試合はありません* 🐨`,
-        },
-      } as SectionBlock]
-    })
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `🐨 *本日の試合はありません* 🐨`,
+          },
+        } as SectionBlock,
+      ],
+    });
     await browser.close();
     return;
   }
 
-  const screenshotBuffer = await element.screenshot();
+  // 試合結果の要素
+  const scoreElement = await page.$('.bb-score');
+  if (!scoreElement) {
+    console.error('Score element is not found');
+    await browser.close();
+    return;
+  }
+  const scoreScreenshotBuffer = await scoreElement.screenshot();
+
+  // 順位表の要素
+  await page.goto('https://baseball.yahoo.co.jp/npb/standings');
+  const standingsElements = await page.$$('.bb-modCommon01');
+  if (standingsElements.length === 0) {
+    console.error('Standings element is not found');
+    await browser.close();
+    return;
+  }
+  const preSeasonStandingsElement = standingsElements[3];
+  const standingsScreenshotBuffer =
+    await preSeasonStandingsElement.screenshot();
+
+  const images = [
+    { imageBuffer: scoreScreenshotBuffer, filename: 'games.png' },
+    { imageBuffer: standingsScreenshotBuffer, filename: 'standings.png' },
+  ];
 
   await postImage({
-    imageBuffer: screenshotBuffer,
-    filename: 'games.png',
-    title: '⚾ 本日の試合はこちら',
-    url,
+    images,
+    link: {
+      title: '⚾ 本日の試合はこちら',
+      url: scoreUrl,
+    },
   });
 }
 
--- src/slack/message.ts
+++ src/slack/message.ts
@@ -1,26 +1,33 @@
-import { Block, WebClient } from '@slack/web-api';
+import { Block, FilesUploadResponse, WebClient } from '@slack/web-api';
 import 'dotenv/config';
 
 const slackToken = process.env.SLACK_TOKEN || '';
 const channelId = process.env.CHANNEL_ID || '';
 
 export async function postImage({
-  imageBuffer,
-  filename,
-  title,
-  url,
+  images,
+  link,
 }: {
-  imageBuffer: Buffer;
-  filename: string;
-  title: string;
-  url: string;
+  images: {
+    imageBuffer: Buffer;
+    filename: string;
+  }[];
+  link: {
+    title: string;
+    url: string;
+  };
 }) {
   const slackClient = new WebClient(slackToken);
-  const uploadResult = await slackClient.files.upload({
-    file: imageBuffer,
-    filename,
-    title,
-  });
+
+  const uploadResults: FilesUploadResponse[] = [];
+  for (const { imageBuffer, filename } of images) {
+    const uploadResult = await slackClient.files.upload({
+      file: imageBuffer,
+      filename,
+      title: filename,
+    });
+    uploadResults.push(uploadResult);
+  }
 
   await slackClient.chat.postMessage({
     channel: channelId,
@@ -29,18 +36,21 @@ export async function postImage({
         type: 'section',
         text: {
           type: 'mrkdwn',
-          text: `*<${url}|${title}>*`,
+          text: `*<${link.url}|${link.title}>*`,
         },
       },
-      {
+      ...uploadResults.map((uploadResult) => ({
         type: 'image',
         image_url: uploadResult.file?.permalink,
-        alt_text: title,
-      },
+        alt_text: uploadResult.file?.title || 'uploaded image',
+      })),
     ],
   });
 
-  console.log('📸 Image posted:', filename);
+  console.log(
+    '📸 Image posted:',
+    images.map((image) => image.filename).join(', ')
+  );
 }
 
 export async function postMessage({ blocks }: { blocks: Block[] }) {
