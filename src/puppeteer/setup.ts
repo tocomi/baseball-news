b732c08c5cd84ba4b9c59f9e47c02b379c37bd2a
tocomi <tocomi0112@gmail.com>
2025-04-02 23:49:08 +0900

    ci: ワークフローを追加

--- src/puppeteer/setup.ts
+++ src/puppeteer/setup.ts
@@ -4,7 +4,7 @@ export async function setup(): Promise<{
   page: Page;
   browser: Browser;
 }> {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
   const page = await browser.newPage();
   console.log('🚀 setup is DONE');
   return { page, browser };
